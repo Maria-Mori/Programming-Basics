c213a0bfdf724cac199850488a708ac990a1ddd7
Maria Mori <43771844+Maria-Mori@users.noreply.github.com>
2018-11-15 09:33:15 +0900

    Update 07-01.cpp

--- 07-01.cpp
+++ 07-01.cpp
@@ -1,10 +1,21 @@
-﻿#include <iostream>
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 // 2つの整数値を小さい順に並び替える
-// 関数 myswap をポインタを引数として作成
+// 関数 myswap をポインタを引数として受け取る
+void myswap(int *a[5] ,int *b[5])
+{
+  int x = 0;
+  if(*a[x] > *b[x])
+      {
+        int temp;
+        temp = *a[x];
+        *a[x] = *b[x];
+        *b[x] = temp;
+      }
+}
 
 int main(void)
 {
@@ -13,9 +24,9 @@ int main(void)
     
     for (int i = 0; i < 5; ++i)
     {
-        // myswap を呼び出して a[i] と b[i] を並び替え
-
-        cout << a[i] << ", " << b[i] << endl;
+     myswap(&a[i], &b[i]);
+                                                   // myswap を呼び出して a[i] と b[i] を並び替え
+     cout << a[i] << ", " << b[i] << endl;
     }
     return 0;
 }
